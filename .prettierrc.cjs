7dc1ffccc8197cbc67c302d22e6484337dff1cdc
Kirill Tagai <tagaykirill83@gmail.com>
2026-03-02 00:28:36 +0500

    test action

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -11,6 +11,11 @@ module.exports = {
         semi: true,
         tabWidth: 2,
       },
+
+      files: ['*.tsx', '*.jsx'],
+      options: {
+        parser: 'typescript'
+      }
     },
   ],
 };
